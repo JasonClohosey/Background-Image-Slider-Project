5f3fb9ab2a5f6cdeb2962b4696f0365186118a83
Jason Clohosey <clohoseyjason@gmail.com>
2020-05-22 12:08:38 -0400

    got slider functioning properly minus it looping back to first image/last image

--- main.js
+++ main.js
@@ -1,21 +1,43 @@
-var image = [
-    "images/pic-1.jpeg",
-    "images/pic-2.jpeg",
-    "images/pic-3.jpeg",
-    "images/pic-4.jpeg",
-    "images/pic-5.jpeg",
+let image = [
+    "assets/images/pic-1.jpeg",
+    "assets/images/pic-2.jpeg",
+    "assets/images/pic-3.jpeg",
+    "assets/images/pic-4.jpeg",
+    "assets/images/pic-5.jpeg",
 ];
+
+var imagesIndex = 0
 console.log(image);
 
-const buttons = document.querySelectorAll(".btn")
+const buttons = document.querySelectorAll(".btn");
+const imageDiv = document.querySelector(".image");
+
+changeImage = () => {
+    currentImage = image[imagesIndex];
+    imageDiv.style.backgroundImage = `url(${currentImage})`;
+    console.log("current image", currentImage);
+  };
 
 buttons.forEach((button) => {
     button.addEventListener("click", function() {
         if (button.classList.contains("backBtn")) {
-            console.log("back clicked");
+            imagesIndex--
+            console.log("imageIndex Should decrease", imagesIndex);
+            changeImage()
         }
         if (button.classList.contains("forwardBtn")) {
-            console.log("forward clicked");
+            imagesIndex++
+            console.log("imageIndex Should increase", imagesIndex);
+            changeImage()
         }
+    // for (let i = 0; i < image.length; i++) {
+    //     const nextImage = image[i];
+    //     console.log(nextImage)
+    //     document.querySelector(".image").style.backgroundImage = `url("images/pic-${nextImage}")`;
+    //     console.log(nextImage)
+    // }
+        
+
+
     });
 });
